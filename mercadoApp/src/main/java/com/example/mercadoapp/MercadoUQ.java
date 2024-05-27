a2030cd3acf2bce4d149f3d0dacea2f3158e8a40
Maria Marmolejo <mariamarmolejo0401@gmail.com>
2024-05-27 00:59:55 -0500

    Añadir hoja de estilo

--- mercadoApp/src/main/java/com/example/mercadoapp/MercadoUQ.java
+++ mercadoApp/src/main/java/com/example/mercadoapp/MercadoUQ.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
 
 public class MercadoUQ extends Application {
 
@@ -18,6 +20,9 @@ public class MercadoUQ extends Application {
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("MercadoUQ");
         stage.setScene(scene);
+
+        // Agregar el archivo CSS a la escena
+        scene.getStylesheets().add( MercadoUQ.class.getResource("css/styles.css").toExternalForm());
       //  stage.initStyle(StageStyle.TRANSPARENT);
         stage.setResizable(false);
       //  Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/image-removebg-preview.png")));
